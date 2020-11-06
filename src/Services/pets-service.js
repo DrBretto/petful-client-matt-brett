a91c0c8f84781bf20fb6a4d2b4087c6433e105e8
Mathew Murray <the.thran@gmail.com>
2020-11-06 14:29:35 -0500

    updated for live hooks

--- src/Services/pets-service.js
+++ src/Services/pets-service.js
@@ -2,14 +2,14 @@ import config from '../config';
 
 const petsApiService = {
   getPets() {
-    return fetch(`${config.API_ENDPOINT}/pets`, {
+    return fetch(`${config.REACT_APP_API_BASE}/pets`, {
       headers: {},
     }).then((res) =>
     !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
   deletePets(pet){
-    return fetch(`${config.API_ENDPOINT}/pets`, {
+    return fetch(`${config.REACT_APP_API_BASE}/pets`, {
       method: 'DELETE',
       headers: {
         'content-type': 'application/json'
